7a226b7c53a6c22643b8843ca7aa2360be5b6372
VJS <minsung_96@naver.com>
2021-04-07 22:52:43 +0900

    commerce 2

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import {Grid} from '@material-ui/core'
-import Product from "./Product/Product"
 
-const productProps = [
+import Product from '../Product/Product'
+import useStyles from './styles'
+
+const products = [
     {id:1, name:'Shoes', description : 'Running Shoes', price:'80,000'},
     {id:2, name:'Macbook', description : 'Apple Macbook', price:'3,500,000'},
     {id:3, name:'Cellphone', description : 'Samsung Galaxy S21', price:'1,200,000'}
 ]
 
 const Products = () => {
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
-                {productProps.map((product) => {
-                    <Grid item key = {productProps.id} xs={12} sm={6} md={4} lg = {3}>
+                {products.map((product) => (
+                    <Grid item key = {products.id} xs={12} sm={6} md={4} lg = {3}>
                         <Product product = {product}/>
                     </Grid>
-                })}
+                ))}
             </Grid>
         </main>
     )
