340107fd1b1887a3b8389c1f011a5690018b0584
VJS <minsung_96@naver.com>
2021-04-20 23:48:37 +0900

    form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -7,7 +7,7 @@ import {commerce} from '../../../lib/commerce'
 
 const steps = ['Shipping Address', 'Payment Details']
 
-const Checkout = ({cart}) => {
+const Checkout = ({cart, order, onCaptureCheckout, error}) => {
     const [activeStep, setActiveStep] = useState(0)
     const [checkoutToken, setCheckoutToken] = useState(null)
     const [shippingData, setShippingData] = useState({})
@@ -43,7 +43,7 @@ const Checkout = ({cart}) => {
 
     const Form = () => activeStep === 0 
         ? <AddressForm checkoutToken = {checkoutToken} next={next}/> 
-        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} prevStep={prevStep}/>
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} prevStep={prevStep} onCaptureCheckout={onCaptureCheckout}/>
 
 
     const ConfirmationForm = () => (
